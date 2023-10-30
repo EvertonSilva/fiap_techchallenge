6914f36947c761d64ddf329bbddaa7ba6c4945ba
Gilberto Silva <bertugil@gmail.com>
2023-10-30 20:51:44 -0300

    remove mapeamento de endpoints de clientes do BaseController

--- api/src/main/java/br/com/edu/fiap/techchallengelanchonete/controller/BaseController.java
+++ api/src/main/java/br/com/edu/fiap/techchallengelanchonete/controller/BaseController.java
@@ -28,33 +28,4 @@ public class BaseController {
         var prod = useCase.getProduto(1L);
         return prod.getNome().getValor();
     }
-
-    @Autowired
-    ClienteUseCase clienteUseCase;
-    @GetMapping("/cliente")
-    public String salvaCliente()
-    {
-        var cliente = clienteUseCase.semIdentificacaoCliente(new Cliente());
-
-        return cliente.getNome().getValor();
-    }
-
-    @PostMapping("/autenticaCliente")
-    public ResponseEntity<Cliente> buscaCliente()
-    {
-        var cliente = new Cliente(new Email("autentica@cliente"), new CPF("11111111122"));
-        cliente = clienteUseCase.autenticaCliente(cliente);
-
-        return ResponseEntity.ok().body(cliente);
-    }
-
-    @PostMapping("/cliente")
-    public ResponseEntity<Cliente> testeCliente()
-    {
-        var cliente = new Cliente(new Nome("Nome cliente 001"), new Email("salva@cliente"), new CPF( "11111111111"));
-        cliente = clienteUseCase.salvaCliente(cliente);
-
-        return ResponseEntity.ok().body(cliente);
-    }
-
 }

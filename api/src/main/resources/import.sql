619f5767594c0bf267d7bfbb15e1571c440ce421
Gilberto Silva <bertugil@gmail.com>
2023-10-30 21:23:10 -0300

    adiciona clienteController, ajuste caso de uso cliente

---
+++ api/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+/* Inserindo categorias */
+INSERT INTO Categoria (nome) VALUES ('Hamburgers');
+INSERT INTO Categoria (nome) VALUES ('Bebidas');
+INSERT INTO Categoria (nome) VALUES ('Acompanhamentos');
+INSERT INTO Categoria (nome) VALUES ('Sobremesas');
+INSERT INTO Categoria (nome) VALUES ('Café da Manhã');

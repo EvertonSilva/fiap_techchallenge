6338f7639b985a208d690de46ad9aab87e39da13
Talles Costa <talles.costa@muralis.com.br>
2023-11-01 20:11:49 -0300

    refactor: capitaliza descrições das categorias

--- api/src/main/resources/import.sql
+++ api/src/main/resources/import.sql
@@ -1,1 +1,5 @@
-INSERT INTO categorias (id, nome) VALUES (1, 'Lanche'), (2, 'Acompanhamento'), (3, 'Bebida'), (4, 'Sobremesa');
+INSERT INTO categorias (id, data_criacao, data_atualizacao, nome)
+VALUES (1, now(), now(), 'LANCHE'),
+        (2, now(), now(), 'ACOMPANHAMENTO'),
+        (3, now(), now(), 'BEBIDA'),
+        (4, now(), now(), 'SOBREMESA');

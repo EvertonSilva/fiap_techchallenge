32a3a9d2e144db3b5557deba2ea13140196b254f
Talles Costa <talles.costa@muralis.com.br>
2023-11-01 13:36:12 -0300

    refactor: torna o cadastro de clientes duplicados uma bad request

--- application/src/main/java/br/com/edu/fiap/techchallengelanchonete/usecase/ClienteUseCase.java
+++ application/src/main/java/br/com/edu/fiap/techchallengelanchonete/usecase/ClienteUseCase.java
@@ -6,6 +6,7 @@ import br.com.edu.fiap.techchallengelanchonete.domain.valueobject.Id;
 import br.com.edu.fiap.techchallengelanchonete.domain.valueobject.CPF;
 import br.com.edu.fiap.techchallengelanchonete.domain.valueobject.Email;
 import br.com.edu.fiap.techchallengelanchonete.domain.valueobject.Nome;
+import br.com.edu.fiap.techchallengelanchonete.exception.ApplicationException;
 import br.com.edu.fiap.techchallengelanchonete.infrastructure.IClientePersistence;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
@@ -27,14 +28,13 @@ public class ClienteUseCase {
 
     public Cliente salvaCliente(Cliente cliente) {
         Cliente clienteExiste = this.clienteRepository.buscaCPF(cliente.getCpf());
-        if(clienteExiste.getId() != null)
-            return clienteExiste;
-        else
-            return this.clienteRepository.cadastro(cliente);
+        if (clienteExiste.getId() != null)
+            throw new ApplicationException("Cliente já existe!");
+
+        return this.clienteRepository.cadastro(cliente);
     }
 
     public Cliente buscaClientePorCPF(String cpf) {
-//        this.teste();
         return this.clienteRepository.buscaCPF(new CPF(cpf));
     }
 

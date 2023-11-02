ff59771d836dbb98d9246597760eb7bbc66c67b8
Talles Costa <talles.costa@muralis.com.br>
2023-11-01 21:49:28 -0300

    refactor: corrige inserção automatica de categorias no startup da aplicação

--- api/src/main/resources/import.sql
+++ api/src/main/resources/import.sql
@@ -1,5 +1,1 @@
-INSERT INTO categorias (id, data_criacao, data_atualizacao, nome)
-VALUES (1, now(), now(), 'LANCHE'),
-        (2, now(), now(), 'ACOMPANHAMENTO'),
-        (3, now(), now(), 'BEBIDA'),
-        (4, now(), now(), 'SOBREMESA');
+INSERT INTO categorias (id, data_criacao, data_atualizacao, nome) VALUES (1, current_timestamp, current_timestamp, 'LANCHE'), (2, current_timestamp, current_timestamp, 'ACOMPANHAMENTO'), (3, current_timestamp, current_timestamp, 'BEBIDA'), (4, current_timestamp, current_timestamp, 'SOBREMESA');
